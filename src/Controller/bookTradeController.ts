d9fca0e31116f1f9bfe91c48431bfba52b64c772
JPedroDev12 <joaopedromes12@gmail.com>
2026-04-17 16:41:29 -0300

    Cria booktradeController

--- src/Controller/bookTradeController.ts
+++ src/Controller/bookTradeController.ts
@@ -1,2 +1,16 @@
 import { Request, Response } from "express"
 import { db } from "../config/knex"
+import { BookTrade } from "../Interface/bookTrade.Interface"
+import { CreateBookTradeDTO, UpdateBookTradeDTO } from "../Dto/bookTrade.dto"
+
+export async function GetBookTrade(res: Response, req: Request) {
+    const book = await db<BookTrade>("book_trade").select("*")
+    return res.status(200).json({book})
+}
+
+export async function GetBookTradeById(res: Response, req: Request) {
+    const id = +req.params.id
+    const book = await db<BookTrade>("book_trade").where({id}).first()
+
+    
+}
