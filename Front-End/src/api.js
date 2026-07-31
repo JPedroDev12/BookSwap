83fbecbcf2e983914a8a316a79bce8415e8efd34
JPedroDev12 <joaopedromes12@gmail.com>
2026-07-31 18:09:24 -0300

    cria host para rede local

---
+++ Front-End/src/api.js
@@ -0,0 +1,18 @@
+import { io } from "socket.io-client";
+
+const BASE_URL = "http://192.168.100.74:3000"; //ip do pc (trocar caso necessário)!!!!!!
+
+export const socket = io(BASE_URL, {
+  autoConnect: true
+});
+
+export const fetchAPI = async (endpoint, options = {}) => {
+  const response = await fetch(`${BASE_URL}${endpoint}`, {
+    headers: {
+      "Content-Type": "application/json",
+      ...options.headers,
+    },
+    ...options,
+  });
+  return response.json();
+};
--- Front-End/src/main.jsx
+++ Front-End/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import "./api.js"
 
 import Inicial from './pages/pInicial';
 import Login from './pages/pLogin';
